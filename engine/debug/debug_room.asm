f2662956edca6a67fbe87e6c6bf087d2e6c75be9
xaerochill <enosnemeis@gmail.com>
2025-12-29 09:31:05 +0100

    Make Debug Room accessible without button combination

--- engine/debug/debug_room.asm
+++ engine/debug/debug_room.asm
@@ -30,10 +30,6 @@ DEF DEBUGROOMMENU_NUM_PAGES EQU const_value
 	const DEBUGROOMMENUITEM_BT_BUG_POKE  ; 14
 
 _DebugRoom:
-	ldh a, [hJoyDown]
-	and PAD_SELECT | PAD_START
-	cp PAD_SELECT | PAD_START
-	ret nz
 	ldh a, [hDebugRoomMenuPage]
 	push af
 	xor a
